ffa5325a19896d4c133974c45d99c849a020f8dc
Armin Özkalp <armin.ozkalp@chasacademy.se>
2026-09-10 14:36:51 +0200

    Remove forwarded NAT firewall rule
    Removed the google_compute_firewall resource for forwarded NAT.

--- main.tf
+++ main.tf
@@ -211,16 +211,5 @@ resource "google_compute_firewall" "allow_forwarded_nat" {
   target_tags   = ["jumphost"]
 }
 
-resource "google_compute_firewall" "allow_forwarded_nat" {
-  name    = "team${var.team_id}-allow-forwarded-nat"
-  network = data.google_compute_network.team_vpc.name
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
-
-  source_ranges = [local.subnet_cidr]
-  target_tags   = ["jumphost"]
-}
 
