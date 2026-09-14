049f119758cd1823953c713e1c66799f5c9b3b49
abdi-ches <hakim.abdi@chasacademy.se>
2026-09-14 11:55:49 +0200

    Aktivera DATA_READ-loggning för Cloud Storage (#60)

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -49,6 +49,16 @@ resource "google_storage_bucket" "terraform_state" {
   }
 }
 
+
+resource "google_project_iam_audit_config" "storage_data_read" {
+  project = var.project_id
+  service = "storage.googleapis.com"
+
+  audit_log_config {
+    log_type = "DATA_READ"
+  }
+}
+
 resource "google_iam_workload_identity_pool" "github" {
   workload_identity_pool_id = "team${var.team_id}-github-pool"
   display_name              = "GitHub Actions Pool"
